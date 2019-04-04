7fdc9e369a7edec12231d0e703ea1c15fa162448
Egor Ptashnik <gogavp@mail.ru>
2019-04-05 00:35:33 +0300

    Add correct responses to client (text or server error)

--- source code/app.py
+++ source code/app.py
@@ -1,26 +1,46 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pho2txt
-import scipy.misc
 import os
 
 app = Flask(__name__)
 CORS(app)
 
 
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
 @app.route('/files', methods=['POST'])
 def make_recognition():
-    image = request.files['image']
+    try:
+        image = request.files['image']
 
-    file = open(image.filename, 'rb+')
-    image.save(dst=file)
-    file.close()
+        file = open(image.filename, 'rb+')
+        image.save(dst=file)
+        file.close()
 
-    output = pho2txt.binarization(image.filename, 'ta')
-    scipy.misc.imsave('output.jpg', output)
-    os.remove(image.filename)
+        text = pho2txt.to_txt(image.filename, 'ta')
+        os.remove(image.filename)
 
-    return image.filename
+        return jsonify(text)
+    
+    except Exception:
+        response = jsonify({"message": "Internal server error"})
+        response.status_code = 500
+        return response
 
 
 if __name__ == '__main__':
